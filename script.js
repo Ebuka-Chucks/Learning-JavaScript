b6528ce65a3fc109521f22e9475a5d0d8945dd9d
Ebuka-Chucks <Chrisbrandford4@gmail.com>
2023-06-13 14:18:21 +0100

    web extraction

---
+++ script.js
@@ -0,0 +1,33 @@
+///Selecting Elements on the webpage = class name, ID, Tagname,CSS selectors
+//CLASS NAME
+// const heading = document.getElementsByClassName("heading");
+// console.log(heading);
+// //ID
+// const para = document.getElementById("ptag");
+// console.log(para);
+// //TAG NAME
+// const p = document.getElementsByTagName("p")
+// console.log(p);
+// //QUERY SELECTORS - CSS SELECTORS -CLASS, #ID, P
+// //QUERRY SELECTORS
+// const paragraph = document.querySelector("p");
+// console.log(paragraph);
+
+// const paragraphs = document.querySelectorAll("p");
+// //
+// paragraphs.forEach((p) => console.log(p));
+// //CLASS
+// const h1 = document.querySelector(".heading");
+// console.log(h1);
+// //#ID
+// const div = document.querySelector("#test");
+// console.log(div);
+
+// //changing content on the webpage- innerText , textContent , innerHTML
+// //INNERTEXT
+// console.log(h1.innerText);
+// //h1.innerText += " Welcome ";
+// console.log(h1.textContent);
+// h1.textContent = " Hello Class"
+// console.log(div.innerHTML);
+
